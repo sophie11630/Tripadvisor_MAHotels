a079e3bad50a289c2aedc3b59e3109e13028b2c4
Yutong(Sophie) Li <71534620+sophie11630@users.noreply.github.com>
2020-12-07 23:06:56 -0500

    Update MA_hotels_rating_prediction.py

--- MA_hotels_rating_prediction.py
+++ MA_hotels_rating_prediction.py
@@ -188,7 +188,7 @@ better_ratio = list(ent_gini_choice_list[:,1])
 better_index = better_ratio.index(max(better_ratio))
 ent_or_gini  = criterion_chosen[better_index]
 best_depth   = int(list(ent_gini_choice_list[:,0])[better_index])
-print("The depth that gives give the best right-to-total ratio is: ", ent_or_gini, ", ", best_depth)
+print("The depth that gives the best right-to-total ratio is: ", ent_or_gini, ", ", best_depth)
     
     
 #use test data to construct conf_matrix
@@ -255,7 +255,7 @@ print(k_dict_knn)
 ratio_list_knn = list(k_dict_knn.values())
 max_index_knn = ratio_list_knn.index(max(ratio_list_knn))
 best_k = list(k_dict_knn.keys())[max_index_knn]
-print("The k that gives give the best right-to-total ratio is: ", best_k)
+print("The k that gives the best right-to-total ratio is: ", best_k)
 
 #use test data to construct conf_matrix
 
